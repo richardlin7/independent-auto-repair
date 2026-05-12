c6c48056118379e7874a3df4eb10d78739b6ed4d
Richard Lin <richard9774lin@gmail.com>
2026-05-12 16:25:01 -0400

    successful mongoDB Atlas connection

--- server/src/main/java/com/autorepair/AutorepairApplication.java
+++ server/src/main/java/com/autorepair/AutorepairApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AutorepairApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AutorepairApplication.class, args);
 	}
-
 }
